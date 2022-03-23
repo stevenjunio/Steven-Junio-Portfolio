87b3a2d6a54d95d01f278fbe4e34bd008bdfbff8
Steven Junio <Steven.junio91@gmail.com>
2022-03-23 08:57:54 -0700

    Added basic api call

--- public/app.js
+++ public/app.js
@@ -1,2 +1,13 @@
 var msg = 'Hello World';
 console.log(msg);
+
+
+fetch('https://xwu6-duwo-frcu.n7.xano.io/api:YOmsVTXu/project')
+.then(
+    
+    response => response.text()
+    
+        
+    
+    
+)
