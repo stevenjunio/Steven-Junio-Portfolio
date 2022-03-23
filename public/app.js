1ce197fc3ad8383e79c2b98a5acc314c86216f41
Steven Junio <Steven.junio91@gmail.com>
2022-03-23 15:14:55 -0700

    Committed

--- public/app.js
+++ public/app.js
@@ -1,13 +1,21 @@
-var msg = 'Hello World';
+var msg = "Hello World";
 console.log(msg);
 
-
-fetch('https://xwu6-duwo-frcu.n7.xano.io/api:YOmsVTXu/project')
-.then(
-    
-    response => response.text()
-    
-        
-    
-    
-)
+fetch("https://xwu6-duwo-frcu.n7.xano.io/api:YOmsVTXu/project").then(
+  (response) =>
+    response.json().then((data) => {
+      console.log(data);
+      let projects = data.projects;
+      console.log(projects);
+      for (const project of projects) {
+        console.log(project.Name);
+        document.createElement("div");
+        document.getElementById("mainPage").innerHTML +=
+          `<div class="projectContainer">
+          <div class ="projectTitle">` +
+          project.Name + '</div>' +
+          project.Description +
+          `</div>`;
+      }
+    })
+);
