93dd7ae532cb02ae86b159c9de4ac249fc5f9b5f
Steven Junio <Steven.junio91@gmail.com>
2022-03-24 07:31:28 -0700

    Return image if image available

--- public/app.js
+++ public/app.js
@@ -1,6 +1,16 @@
 var msg = "Hello World";
 console.log(msg);
 
+let checkImage = (image) => {
+  if (image != null) {
+    console.log(image.path + "image");
+    return `<img class ="projectImage" height ="${image.meta.height}" width="${image.meta.width}" src ="https://xwu6-duwo-frcu.n7.xano.io` + `${image.path}"></img>`;
+    } else {
+      return "";
+    }
+  }
+
+
 fetch("https://xwu6-duwo-frcu.n7.xano.io/api:YOmsVTXu/project").then(
   (response) =>
     response.json().then((data) => {
@@ -9,13 +19,16 @@ fetch("https://xwu6-duwo-frcu.n7.xano.io/api:YOmsVTXu/project").then(
       console.log(projects);
       for (const project of projects) {
         console.log(project.Name);
-        document.createElement("div");
+
         document.getElementById("mainPage").innerHTML +=
-          `<div class="projectContainer">
-          <div class ="projectTitle">` +
-          project.Name + '</div>' +
+          `<div class="projectContainer">` +
+          checkImage(project.Image) +
+           
+          `<div class="projectTitle" > ` +
+          project.Name +
+          "</div>" +
           project.Description +
-          `</div>`;
+          ` < /div>`;
       }
     })
 );
